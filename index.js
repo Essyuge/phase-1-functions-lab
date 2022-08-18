b806d88709d0027d8f4fb6032c5289e6423046e2
EstherWanjiruUge <ugeesther@gmail.com>
2022-08-18 15:16:09 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const scubeHeadquarters = 42;
+ function distanceFromHqInBlocks( a, b= scubeHeadquarters) {
+    return Math.abs(a-b);
+  }
+
+ function distanceFromHqInFeet(a, b) {
+    return(distanceFromHqInBlocks(a,b)*264)
+
+ }
+ function distanceTravelledInFeet(a, b){
+    return(distanceFromHqInFeet(a,b))
+
+ }
+ function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    if(distanceInFeet <= 400){
+      return 0
+    } else if(distanceInFeet > 400 && distanceInFeet <= 2000){
+      const chargableDistance = distanceInFeet - 400;
+      return chargableDistance * 2/100
+    } else if(distanceInFeet > 2000 && distanceInFeet <= 2500){
+      return 25
+    } else if(distanceInFeet > 2500){
+      return 'cannot travel that far'
+    }
+  }
